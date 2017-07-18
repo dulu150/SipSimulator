ce0ba1269ba4a1466a277976ca087e4689da64f3
z00293707 <zhanghaoping@huawei.com>
2017-07-18 19:22:03 +0800

    增加跟踪解析功能,暂时只完成跟踪消息单元处理功能

--- SipSimulator/SIPMmessageAssembler.cs
+++ SipSimulator/SIPMmessageAssembler.cs
@@ -55,5 +55,102 @@ namespace SipSimulator
             sipMessageAfterMod +=  Environment.NewLine;
             return sipMessageAfterMod;
         }
+
+        public static void ConverPtmfToSipMsg(string filepath, string callerIp, string calleeIp)
+        {
+            if (!File.Exists(filepath)) Log.PrintTrace("Ptmf file doesn't exist!!!", CallRole.Invalidrole);
+
+        }
+    }
+
+    class SIPMessageUnit
+    {
+        public CallRole role;
+        public MsgDirection msgdirect;
+        public string messagecontent;
+        public string messageType;
+        public string localIp;
+
+        public void SIPMessageUnit()
+        {
+            this.Clear();
+        }
+
+        public void Clear()
+        {
+            role = CallRole.Invalidrole; 
+            msgdirect = MsgDirection.INVALIDMSGDIRECTION; 
+            messagecontent = "";
+            messageType = "";
+            localIp = "";
+        }
+
+        public void Set(CallRole role, MsgDirection msgdirect, string messagecontent, string messageType, string localIp)
+        {
+            SetRole(role);
+            SetMsgDirection(msgdirect);
+            SetMessageContent(messagecontent);
+            SetMessageType(messageType);
+            SetLocalIp(localIp);
+        }
+
+        public void SetRole(CallRole role)
+        {
+            this.role = role;
+        }
+
+        public void SetMsgDirection(MsgDirection msgdirect)
+        {
+            this.msgdirect = msgdirect;
+        }
+
+        public void SetMessageContent(string messagecontent)
+        {
+            this.messagecontent = messagecontent;
+        }
+
+        public void SetMessageType(string messageType)
+        {
+            this.messageType = messageType;
+        }
+
+        public void SetLocalIp(string localIp)
+        {
+            this.localIp = localIp;
+        }
+
+        public void Export()
+        {
+            if (IsValid() == false) return;
+
+            AddToMsgPlan();
+            AddToMsgBluePrints();
+        }
+
+        void AddToMsgPlan()
+        {
+            File.AppendAllText(role.ToString() + "MsgPlan.txt", msgdirect.ToString() + " " + role.ToString() + "_" + messageType + ".txt");
+        }
+
+        void AddToMsgBluePrints()
+        {
+            File.AppendAllText(role.ToString() + "MsgBluePrints\\" + role.ToString() + "_" + messageType + ".txt", messagecontent);
+        }
+
+        bool IsValid()
+        {
+            if (role >= CallRole.Invalidrole) return false;
+
+            if (msgdirect >= MsgDirection.INVALIDMSGDIRECTION) return false;
+
+            if (messagecontent.IndexOf("Call-ID:") < 0) return false;
+
+            if (messageType.Length <= 0) return false;
+
+            if (localIp.Split(new char[] { '.' }).Length != 3) return false;
+
+            return true;
+        }
+
     }
 }
